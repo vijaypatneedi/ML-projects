af029ff5dd5cba84ca92aea264de3baccb7b102b
vijaypatneedi <vijaykumarpatneedi@gmail.com>
2020-04-28 16:16:18 +0530

    created entry point /about

--- flask/tutorial1.py
+++ flask/tutorial1.py
@@ -1,15 +1,14 @@
-import flask
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
 def hello():
-    return 'Hello, World!'
 
-@app.route("/vijay")
-def harry():
-    return "Hello vijay, keep going...!"
+    return render_template('index.html')
 
-app.run(debug=True) #detects change and reloads
+@app.route("/about")
+def about():
+    name = "vijay"
+    return render_template('about.html', name2= name)
+app.run(debug=True)
+
