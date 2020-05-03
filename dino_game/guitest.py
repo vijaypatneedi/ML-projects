0e3e86f36119dcfa816a8fd311054e38363769f9
vijaypatneedi <vijaykumarpatneedi@gmail.com>
2020-05-03 23:51:07 +0530

    has bugs_to be resolved

--- dino_game/guitest.py
+++ dino_game/guitest.py
@@ -1,23 +1,45 @@
 import pyautogui
 from PIL import Image, ImageGrab
+#from numpy import asarray
 import time
 
 def hit(key):
     pyautogui.keyDown(key)
+    return
 
-#def draw():
+def isCollide(data):
+    for i in range(270,335):
+         for j in range(543,650):
+            if data[i,j]<100:
+                hit("up")
+                return 
+
+    for i in range(250,300):
+         for j in range(410,543):              
+            if data[i,j]<171:
+                hit("down")
+                return 
+    return 
+
     
 
-def takeScreenshot():
-    image=ImageGrab.grab()
-    image.show()
-    return image
-
 if __name__=="__main__":
-    time.sleep(1)
-    image= takeScreenshot()
-    data= image.load()
-    print(data)
-    for i in range(34,45):
-        for j in range(45,67):
-            data
+    print("Game starts in 3sec")
+    time.sleep(3)
+    hit("up")
+    while True:
+        image=ImageGrab.grab().convert("L")
+        data= image.load()
+        isCollide(data)
+        
+        ###print (asarray(image))
+        # for i in range(270,315):
+        #     for j in range(543,650):
+        #         data[i,j]=0
+
+        # for i in range(250,300):
+        #     for j in range(410,543):
+        #         data[i,j]=171
+
+        # image.show()
+        # break
